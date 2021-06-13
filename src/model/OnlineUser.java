3ef97d7ee8e8c4134c7689c3cf0fff244d53ec64
rachel-lly <3556753750@qq.com>
2021-06-13 11:17:07 +0800

    OnlineUser

--- src/model/OnlineUser.java
+++ src/model/OnlineUser.java
@@ -5,14 +5,14 @@ import java.nio.channels.AsynchronousSocketChannel;
 
 public class OnlineUser {
     public final User user;
-    public AsynchronousSocketChannel sc;
+    public AsynchronousSocketChannel socketChannel;
 
     public String publicKey;
 
     public String privateKey;
 
-    public OnlineUser(AsynchronousSocketChannel sc, String publicKey, String privateKey, User user) {
-        this.sc = sc;
+    public OnlineUser(AsynchronousSocketChannel socketChannel, String publicKey, String privateKey, User user) {
+        this.socketChannel = socketChannel;
         this.publicKey = publicKey;
         this.privateKey = privateKey;
         this.user = user;
